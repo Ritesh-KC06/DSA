2f9b1a4104aeef5a75107ba5501d3c2de4349811
Ritesh-KC06 <riteshkc06@gmail.com>
2024-05-31 23:46:18 +0530

    First Trial  Code

---
+++ IntroToProblemSolving/CountFactors2.java
@@ -0,0 +1,12 @@
+package DSA.IntroToProblemSolving;
+
+//import java.lang.*;
+//import java.util.*;
+
+public class CountFactors2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
