73d3b589b50449269ea624016111f1c5af993f5e
Ritesh Chavan <riteshc06@outlook.com>
2024-11-13 16:20:39 +0530

    Assignment problems on Function 1

--- IntroToProblemSolving/functions/Ffunction.java
+++ IntroToProblemSolving/functions/Ffunction.java
@@ -8,4 +8,32 @@ public class Ffunction {
         return C;
         
     }
+
+    public static int multiply (int A, int B) {
+
+        int C = A * B;
+        return C;
+    }
+
+    public static int square (int A) {
+        int B = A * A;
+        return B;
+    }
+
+    public static int cube (int A) {
+        int B = A * A * A;
+        return B;
+    }
+
+    public static int quotient (int A, int B) {
+        int C = A/B;
+        return C;
+    }
+
+    public static double temperature (double celcius) {
+        double F = ((celcius * 9) / 5) + 32;
+        double ans = Math.round(F * 100) / 100d;
+        return ans;
+    }
+    
 }
--- IntroToProblemSolving/functions/FunctionMain.java
+++ IntroToProblemSolving/functions/FunctionMain.java
@@ -4,11 +4,39 @@ import java.util.*;
 public class FunctionMain {
     public static void main(String[] args) {
         
+        @SuppressWarnings("resource")
         Scanner scn = new Scanner(System.in);
+        System.out.println("Input 1");
         int A = scn.nextInt();
+        System.out.println("Input 2");
         int B = scn.nextInt();
 
+        //Q1 ->
         int C = Ffunction.sum (A, B);
         System.out.println("Sum of " + A + " & " + B + " = " + C);
+
+        //Q2 ->
+        int D = Ffunction.multiply(A, B);
+        System.out.println("Product of " + A + " & " + B + " = " + D);
+
+        //Q3 ->
+        int E = Ffunction.square(A);
+        System.out.println("Square of " + A + " = " + E);
+
+        //Q4 ->
+        int F = Ffunction.cube(A);
+        System.out.println("Cube of " + A + " = " + F);
+
+        //Q5 ->
+        int G = Ffunction.quotient(A, B);
+        System.out.println("Quotient of " + A + " / " + B + " = " + G);
+
+        //Q6 ->
+        System.out.println("Temperature in Celcius");
+        double celcius = scn.nextDouble();
+
+        double Fahrenheit = Ffunction.temperature(celcius);
+        System.out.println("Temperature in Fahrenheit is " + Fahrenheit);
+
     }
 }
