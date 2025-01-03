b47f326ea2ddcf73fecdde51941f8abc7fc3659f
Ritesh Chavan <riteshc06@outlook.com>
2025-01-03 16:09:11 +0530

    1D Array-1 Assignment problems

--- IntroToProblemSolving/ODArray/ODArrayMain1.java
+++ IntroToProblemSolving/ODArray/ODArrayMain1.java
@@ -22,9 +22,40 @@ public class ODArrayMain1 {
             System.out.println(arr[i]);
         }
 
+        /* 
+        //Q3 ->
         int SM = ODArrayFunction.Sum(arr);
-        double avg = ODArrayFunction.Avg(arr, SM);
+        System.out.println("Sum of the array is = " + SM);
 
+        double avg = ODArrayFunction.Avg(arr, SM);
         System.out.println("Average of the array is = " + avg);
+
+        //Q2 ->
+        int max = ODArrayFunction.Max(arr);
+        System.out.println("Highest int of the array is = " + max);
+
+        int min = ODArrayFunction.Min(arr);
+        System.out.println("lowest int of the arra is = " + min);
+
+        //Q4 ->
+        int prod = ODArrayFunction.product(arr);
+        System.out.println("Product of the integers of the array = " + prod);
+
+        //Q1 ->
+        ODArrayFunction.ReverseArray(arr);
+        
+
+        //Q5 ->
+        ODArrayFunction.PrintNegative(arr);
+        */
+        
+        //Q6 ->
+        System.out.println("Please provide integer B for new Array =");
+        int B = scn.nextInt();
+        int [] ARB = ODArrayFunction.NewArrayB(arr, B);
+        System.out.println("new Array = ");
+        for (int i = 0; i < ARB.length; i++){
+            System.out.println(ARB[i]);
+        }
     }
 }
